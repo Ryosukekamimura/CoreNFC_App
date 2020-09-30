c9ed54640e225df7e9e142b14a9998265915c4ab
RyosukeKamimura <come1234560@gmail.com>
2020-09-30 19:30:07 +0900

    Initial Commit

---
+++ UseCoreNFC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UseCoreNFC
+//
+//  Created by 神村亮佑 on 2020/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
